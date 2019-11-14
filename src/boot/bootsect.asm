796bee775aadc80fca9565c65a4e2ccebd808047
Davide Della Giustina <davide@dellagiustina.com>
2019-11-14 13:02:41 +0100

    Hello world from the boot sector

--- src/boot/bootsect.asm
+++ src/boot/bootsect.asm
@@ -3,6 +3,20 @@
 ; @date     14/11/2019
 
 [bits 16]
+
+mov ah, 0x0e
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, '!'
+int 0x10
+
 jmp $
 
 times 510-($-$$) db 0
